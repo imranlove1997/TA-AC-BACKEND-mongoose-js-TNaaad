7122b11820d35861817d6dc9b2ebe0c57554b8a3
Imran Khan <imranlove1997@gmail.com>
2021-12-06 17:33:36 +0530

    Complete

---
+++ block-BNaacj/code/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+
+mongoose.connect("mongodb:localhost/code", (err) => {
+    console.log(err ? err : ('Connected...'));
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome');
+})
+
+app.listen(3000, () => {
+    console.log('Server listenere at port 3000');
+})
